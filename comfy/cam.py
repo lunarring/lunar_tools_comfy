229de9fedf1b06dfacd01a9ff3fc48ba03efc11a
Your Name <jsdmail@gmail.com>
2024-10-25 13:09:18 +0100

    camera autofocus and manual focus value

--- comfy/cam.py
+++ comfy/cam.py
@@ -1,9 +1,13 @@
 import lunar_tools as lt
+import cv2
 import numpy as np
 
 class LRWebCam:
     DEFAULT_CAM_ID = 0
     DEFAULT_HEIGHT = 576
+    DEFAULT_WIDTH = 1024
+    DEFAULT_AUTOFOCUS = True
+    DEFAULT_MANUALFOCUSVAL = 0
     DEFAULT_WIDTH = 1024
     RETURN_TYPES = ("IMAGE", )
     RETURN_NAMES = ("Webcam Image", )
@@ -18,6 +22,9 @@ class LRWebCam:
     def __init__(self):
         self.parameters = ["height", "width", "cam_id"]
         self.cam = None
+        self.autofocus = self.DEFAULT_AUTOFOCUS
+        self.manualfocusval = self.DEFAULT_MANUALFOCUSVAL
+        self.cam_focus = self.DEFAULT_MANUALFOCUSVAL
     
     def initialize_once(self, cam_id, height, width):
         if self.cam is None:
@@ -25,13 +32,21 @@ class LRWebCam:
             self.height = int(height)
             self.width = int(width)
             self.cam = lt.WebCam(cam_id=cam_id, shape_hw=(self.height, self.width))
-        else:
-            for param in self.parameters:
-                if getattr(self, param) != locals()[param]:
-                    print(f"{param} cannot change after initialization!")
+            
+            self.cam.cam.set(cv2.CAP_PROP_AUTOFOCUS, 1 if self.DEFAULT_AUTOFOCUS else 0)
+            self.cam.cam.set(cv2.CAP_PROP_FOCUS, self.DEFAULT_MANUALFOCUSVAL)
+            
+    def update_params(self, autofocus, cam_focus):
+        if self.autofocus != autofocus:
+            self.autofocus = autofocus
+            self.cam.cam.set(cv2.CAP_PROP_AUTOFOCUS, 1 if self.autofocus else 0)
+        if self.cam_focus != cam_focus:
+            self.cam_focus = cam_focus
+            self.cam.cam.set(cv2.CAP_PROP_FOCUS, self.cam_focus)
 
-    def get_img(self, cam_id, height, width):
+    def get_img(self, cam_id, height, width, autofocus, cam_focus):
         self.initialize_once(cam_id, height, width)
+        self.update_params(autofocus, cam_focus)
         
         img = self.cam.get_img()
         img = [img]
@@ -59,6 +74,16 @@ class LRWebCam:
                     "default": cls.DEFAULT_WIDTH, 
                     "min": 0,
                     "max": 4096,
+                    "step": 1,
+                    "display": "number"
+                }),
+                "autofocus": ("BOOLEAN", {
+                    "default": True,
+                }),
+                "manual focus value": ("FLOAT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": 255,
                     "step": 1,
                     "display": "number"
                 }),
